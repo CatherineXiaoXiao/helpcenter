67693fc490ffc68c8c7dbf54e449fbe2410869ee
Catherine Xiao <catherienangel@163.com>
2025-04-07 10:04:06 +0800

    fix: update docusaurus config for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
   baseUrl: '/helpcenter/',
   organizationName: 'CatherineXiaoXiao',
   projectName: 'helpcenter',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
