043c722f5899b2e88b73ebed650c701da1623c10
Leven-hub <levenjangjj@gmail.com>
2025-06-02 19:57:58 +0800

    Update TickList.jsx

--- src/components/TickList.jsx
+++ src/components/TickList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ListStyles.css';
 
-export const TickList = ({ children }) => {
+const TickList = ({ children }) => {
   return <ul className="tick-list">{children}</ul>;
 };
+export default TickList;
